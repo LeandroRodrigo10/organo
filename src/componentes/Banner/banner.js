8d7b2bd7a95f42db59b46591ac88766d2fbc7018
Leandro Rodrigo <117653010+LeandroRodrigo10@users.noreply.github.com>
2024-10-22 14:45:30 -0300

    estilização

--- src/componentes/Banner/banner.js
+++ src/componentes/Banner/banner.js
@@ -1,6 +1,12 @@
+import "./banner.css"
+
 function Banner() {
+
     return(
+
+        <header className="banner">
         <img src="/imagens/banner.png" alt="Banner Principal" />
+        </header>
     )
 }
 
