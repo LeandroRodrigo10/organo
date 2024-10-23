139c6ffd6939586461e1524121f874e354da6678
Leandro Rodrigo <117653010+LeandroRodrigo10@users.noreply.github.com>
2024-10-23 15:19:17 -0300

    cards atualizados

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,11 +1,11 @@
 import "./Colaborador.css"
 
 const Colaborador =() => {
-    return(<div>
-        <div>
+    return(<div className="colaborador">
+        <div className="cabecalho">
             <img src="https://github.com/leandrorodrigo10.png" alt="Leandro usando um computador"/>
         </div>
-        <div>
+        <div className="rodape">
             <h4>Leandro Rodrigo</h4>
             <h5>Front-End</h5>
         </div>
