f1e47be06ab2a8e5160715b4d4d382e7b7dfcc7b
Leandro Rodrigo <117653010+LeandroRodrigo10@users.noreply.github.com>
2024-10-23 15:16:40 -0300

    Colaboradores atualizado até a minha foto

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,16 @@
+import "./Colaborador.css"
+
+const Colaborador =() => {
+    return(<div>
+        <div>
+            <img src="https://github.com/leandrorodrigo10.png" alt="Leandro usando um computador"/>
+        </div>
+        <div>
+            <h4>Leandro Rodrigo</h4>
+            <h5>Front-End</h5>
+        </div>
+    </div>)
+
+}
+
+export default Colaborador
