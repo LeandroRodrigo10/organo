fe4d05c746c9f1605cd8348bb3560b485ca70bf7
Leandro Rodrigo <117653010+LeandroRodrigo10@users.noreply.github.com>
2024-10-24 10:02:19 -0300

    rodape "corrigido"

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,11 +1,11 @@
-import "./Rodape.css"
+import "./Rodape.css";
 
-function Rodape () {
-    return(
+function Rodape() {
+    return (
         <footer className="rodape">
-        <img src="/imagens/rodape.png" alt="rodape da pagina" />
+            <img src="/imagens/rodape.png" alt="rodape da pagina" />
         </footer>
-    )
+    );
 }
 
-export default Rodape
+export default Rodape;
